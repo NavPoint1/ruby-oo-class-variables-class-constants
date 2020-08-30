e232fad1ef8e2414ebee7ccc6bf9f91f55eb16bb
NavPoint1 <foleym156@gmail.com>
2020-08-30 14:46:23 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = ["Uggs", "Rainbow"]
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.any? { |known_brand| known_brand == brand }
+      BRANDS << brand
+    end
   end
 
   def cobble
